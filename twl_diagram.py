0a0053d641912aeba1a4db177103f4beec11432a
Hannes Watolla <watollah@gmail.com>
2024-06-24 17:54:59 +0200

    Fixed bug with positioning of label bg when moving shapes in diagram

--- twl_diagram.py
+++ twl_diagram.py
@@ -147,7 +147,7 @@ class ComponentShape(Generic[C], Shape, Observer):
     def update_label_pos(self):
         label_pos = self.label_position
         self.tk_shapes[self.label_tk_id] = Polygon(label_pos)
-        self.diagram.moveto(self.label_tk_id, label_pos.x, label_pos.y)
+        self.diagram.coords(self.label_tk_id, label_pos.x, label_pos.y)
         x1, x2, y1, y2 = self.diagram.bbox(self.label_tk_id)
         self.tk_shapes[self.label_bg_tk_id] = Polygon(Point(x1, x2), Point(y1, y2))
 
