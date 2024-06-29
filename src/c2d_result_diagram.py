dc3d4fd712ab0bb8634a75a5d93661db3fc00af9
Hannes Watolla <watollah@gmail.com>
2024-06-29 12:54:16 +0200

    Fixed bug with 0 max strength in result diagram

--- src/c2d_result_diagram.py
+++ src/c2d_result_diagram.py
@@ -33,7 +33,7 @@ class BeamForcePlotShape(ComponentShape[Beam]):
         p1 = Point(self.component.start_node.x, self.component.start_node.y)
         p2 = Point(self.component.end_node.x, self.component.end_node.y)
         line = Line(Point(p1.x, p1.y), Point(p2.x, p2.y))
-        height = (self.force.strength / self.get_max_strength()) * self.MAX_HEIGHT
+        height = 0 if self.get_max_strength() == 0 else (self.force.strength / self.get_max_strength()) * self.MAX_HEIGHT
         angle = (self.component.angle + 90) % 360
         if 0 <= angle <= 90 or 270 < angle <= 360:
             angle = (angle + 180) % 360
